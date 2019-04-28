780d89aeada769c61b7e67c94b396d8f1f29e152
MartinRamirez98 <a20150178@pucp.pe>
2019-04-28 10:45:18 -0500

    pantalla modal para agregar autoEval

--- app/modules/curso/ActividadController.js
+++ app/modules/curso/ActividadController.js
@@ -1,9 +1,22 @@
 app.controller('ActividadController',function($scope, $location, $cookieStore, serviceUtil){ 
 
+    $scope.mostrarFila=false;
+    $scope.listaPreg=[];
+
     $scope.btnCalificaciones = function(){
         $location.path("calificaciones")
     }
     $scope.btnAutoEvaluacion=function(){
-        $location.path("autoeval")
+        $('#mdCrearAutoEval').appendTo("body").modal('show');
     }
+    $scope.showFila=function(){
+        $scope.mostrarFila=true;
+        let auxLista={
+            familia:"",
+            pregunta:""
+        };
+        $scope.listaPreg.push(auxLista);
+    }  
+
+
 })
