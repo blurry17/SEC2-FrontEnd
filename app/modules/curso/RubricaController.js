9cbcae3dd888439663b2fc07dfe73c69d1d0d0ec
ManuelTheDev <manuel.delgado.alba@gmail.com>
2019-05-01 10:48:45 -0500

    Rubrica completo - Faltan algunas validaciones

--- app/modules/curso/RubricaController.js
+++ app/modules/curso/RubricaController.js
@@ -8,6 +8,7 @@ app.controller('RubricaController',function($scope, $location, $cookieStore, ser
 
     $("[data-toggle=tooltipOcultarAspecto]").tooltip();
 
+    /* Inicializando la lista de aspectos */
     $scope.lstAspectos = [
             {   
                 nomAspecto: '',
@@ -24,28 +25,43 @@ app.controller('RubricaController',function($scope, $location, $cookieStore, ser
             }
     ]
 
+    /* Funciones Rubrica */
     $scope.btnCrearRubrica = function(){
-        $scope.mostrarCrearRubrica = true;
-        //Tengo que limpiar los datos
-        document.getElementById("nomRubrica").value = ""
-        $scope.lstAspectos.length = 1
-        var aspecto = $scope.lstAspectos[0];
-        aspecto.nomAspecto = ''
-        aspecto.descripcion = ''
-        aspecto.puntajeMax = null
-        aspecto.lstIndicadores.length = 1;
-        var indicador = aspecto.lstIndicadores[0];
-        indicador.nomIndicador = ''
-        indicador.descripcion = ''
-        indicador.puntajeMax = null
-
+        /* Preguntar si desea crear una nueva rúbrica */
+        result = window.confirm('¿Desea crear una nueva rúbrica?');
+        if (result){
+            $scope.mostrarCrearRubrica = true;
+            /* Limpiar datos de lstAspectos */
+            document.getElementById("nomRubrica").value = ""
+            $scope.lstAspectos.length = 1
+            var aspecto = $scope.lstAspectos[0];
+            aspecto.nomAspecto = ''
+            aspecto.descripcion = ''
+            aspecto.puntajeMax = null
+            aspecto.lstIndicadores.length = 1;
+            var indicador = aspecto.lstIndicadores[0];
+            indicador.nomIndicador = ''
+            indicador.descripcion = ''
+            indicador.puntajeMax = null
+        }
     }
 
     $scope.btnGuardarRubrica = function(){
-        $scope.mostrarCrearRubrica = false;
-        window.alert("Se guardó la Rúbrica!")
+        if (document.getElementById('nomRubrica').value == '')
+            window.alert("Ingrese el nombre de la rúbrica!")
+        else{
+            $scope.mostrarCrearRubrica = false;
+            window.alert("Se guardó la Rúbrica!")
+        }
+        
     }
 
+    $scope.btnVerRubricaActual = function(){
+        //Falta: Validar que primero haya guardado la rúbrica
+       $scope.mostrarCrearRubrica = true;
+    }
+
+    /* Funciones Aspectos */
      $scope.btnAgregarAspecto= function() {
         $scope.lstAspectos.push({
             /* datos del aspecto */
@@ -63,6 +79,16 @@ app.controller('RubricaController',function($scope, $location, $cookieStore, ser
         });
      }
 
+    $scope.btnMostrarAspecto = function(aspecto){
+        aspecto.mostrar = !(aspecto.mostrar);
+    }
+
+    $scope.btnQuitarAspecto = function(aspecto){
+        var pos = $scope.lstAspectos.indexOf(aspecto)
+        $scope.lstAspectos.splice(pos,1)
+    }
+
+    /* Funciones Indicadores */
     $scope.btnAgregarIndicador= function(aspecto) {
         aspecto.lstIndicadores.push({  
             /* datos del indicador */
@@ -72,35 +98,9 @@ app.controller('RubricaController',function($scope, $location, $cookieStore, ser
         });
      }
 
-    
-
-    $scope.btnMostrarAspecto = function(aspecto){
-        aspecto.mostrar = !(aspecto.mostrar);
-    }
-
-    $scope.btnQuitarAspecto = function(aspecto){
-        var pos = $scope.lstAspectos.indexOf(aspecto)
-        $scope.lstAspectos.splice(pos,1)
-    }
-    
-
-
-    $scope.btnQuitarIndicador = function(aspecto,indicador){
+     $scope.btnQuitarIndicador = function(aspecto,indicador){
         var pos = aspecto.lstIndicadores.indexOf(indicador)
         aspecto.lstIndicadores.splice(pos,1)
-    }
-
-    $scope.btnVerRubricaActual = function(){
-        /*  
-        if(($scope.lstAspectos.length = 1 && $scope.lstAspectos[0].nomAspecto != "") || $scope.lstAspectos.length > 1)
-            $scope.mostrarCrearRubrica = true;
-        else{
-            window.alert("Primero debe guardar una rúbrica!");
-        }
-        */
-       $scope.mostrarCrearRubrica = true;
-        
-
     }
 
 })
