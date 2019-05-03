49126196b9e7faa444a8b363f86bef7d1e034fd2
Enrique Huamanyauri <ehuamanyauri@pucp.edu.pe>
2019-05-03 13:28:25 -0500

    Nada importante

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('appFS', ['ng', 'ngRoute', 'ngCookies','ngMessages','ServiceCRUD','ServiceUtil']);
+var app = angular.module('appFS', ['ng', 'ngRoute', 'ngCookies', 'ServiceCRUD', 'ServiceUtil']);
 
 app.config(function ($routeProvider, $httpProvider) {
     $httpProvider.defaults.headers.post = { 'Content-Type': 'application/json; charset=utf-8' };
