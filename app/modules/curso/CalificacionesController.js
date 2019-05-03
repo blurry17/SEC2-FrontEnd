d3f4857d68653af144e4f80c70a7279004da2cc9
blurry17 <ehuamanyauri@pucp.edu.pe>
2019-05-02 20:34:41 -0500

    Intento de descargar archivo

--- app/modules/curso/CalificacionesController.js
+++ app/modules/curso/CalificacionesController.js
@@ -1,8 +1,8 @@
-app.controller('CalificacionesController',function($scope, $location, $cookies, serviceUtil){ 
+app.controller('CalificacionesController',function($scope, $location, $cookies, serviceUtil, serviceCRUD){ 
     $scope.usuario = $cookies.getObject('usuario');
     if ($scope.usuario == undefined) $location.path('/');
-    $scope.curso=$cookies.getObject("cursoActual")
-    $scope.actividad=$cookies.getObject("actividadActual")
+    $scope.curso = $cookies.getObject("cursoActual")
+    $scope.actividad = $cookies.getObject("actividadActual")
 
     $scope.irActividad = function(){
         $location.path("actividad")
@@ -10,4 +10,12 @@ app.controller('CalificacionesController',function($scope, $location, $cookies, 
     $scope.irCurso = function(){
         $location.path("curso")
     }
+
+    $scope.btnclick = function(){
+        var file = document.getElementById('file').files;
+        console.dir(file);
+    }
+
+    var url = 'https://paideia.pucp.edu.pe/cursos/mod/resource/view.php?id=381468';
+    //document.getElementById('my_iframe').src = url;
 })
