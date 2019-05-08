4c6c38b57897b2c49572ac5552868436e1bae6ec
Enrique Huamanyauri <ehuamanyauri@pucp.edu.pe>
2019-05-08 18:24:38 -0500

    Implementa servicio de crear rubrica

--- app/modules/main/MainController.js
+++ app/modules/main/MainController.js
@@ -1,4 +1,4 @@
-app.controller('MainController', function ($rootScope, $scope, $location, $window) {
+app.controller('MainController', function ($rootScope, $scope, $location, $cookies, $window) {
     $scope.usuario = $cookies.getObject('usuario');
     if ($scope.usuario == undefined) $location.path('/');
 })
