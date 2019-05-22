4131eeaa94081e5cd3011337292bcdb39f19514e
ManuelTheDev <mjdelgado@pucp.pe>
2019-05-21 21:12:45 -0500

    cambios rubrica

--- app/modules/curso/ActividadController.js
+++ app/modules/curso/ActividadController.js
@@ -1,13 +1,13 @@
-app.controller('ActividadController',function($scope, $location, $cookies,serviceCRUD, serviceUtil){ 
+app.controller('ActividadController', function ($scope, $location, $cookies, serviceCRUD, serviceUtil) {
     $scope.usuario = $cookies.getObject('usuario');
     //if ($scope.usuario == undefined) $location.path('/');
-    $scope.curso=$cookies.getObject("cursoActual")
-    $scope.actividad=$cookies.getObject("actividadActual")
-    $scope.mostrarFila=false;
-    $scope.mostrarPreg=false
-    $scope.listaFam=[];
+    $scope.curso = $cookies.getObject("cursoActual")
+    $scope.actividad = $cookies.getObject("actividadActual")
+    $scope.mostrarFila = false;
+    $scope.mostrarPreg = false
+    $scope.listaFam = [];
 
-    $scope.ejemplo=[/*{
+    $scope.ejemplo = [/*{
         familia:"Preparacion",
         listaPreg:[
             {
@@ -44,55 +44,55 @@ app.controller('ActividadController',function($scope, $location, $cookies,servic
         ],
     }*/];
 
-    $scope.btnCalificaciones = function(){
+    $scope.btnCalificaciones = function () {
         $location.path('calificaciones');
     }
 
-    $scope.btnGrupos = function(){
+    $scope.btnGrupos = function () {
         $location.path('grupos');
     }
 
-    $scope.btnAutoEvaluacion=function(){
+    $scope.btnAutoEvaluacion = function () {
         $('#mdCrearAutoEval').appendTo("body").modal('show');
     }
-    
-    $scope.btnVerAutoEval=function(){
-        params={
-            idActividad:1,
+
+    $scope.btnVerAutoEval = function () {
+        params = {
+            idActividad: 1,
         };
-        serviceCRUD.TypePost("auto-evaluacion/listarPreguntas",params).then(function(response){
+        serviceCRUD.TypePost("auto-evaluacion/listarPreguntas", params).then(function (response) {
             console.dir(response.data);
-            $scope.ejemplo=response.data.listaFamilia;
+            $scope.ejemplo = response.data.listaFamilia;
         })
 
         $('#mdVerAuto').appendTo("body").modal('show');
     }
-    $scope.showFila=function(){
-        $scope.mostrarFila=true;
-        let auxLista={
-            familia:"",
-            listaPregunta:[],
+    $scope.showFila = function () {
+        $scope.mostrarFila = true;
+        let auxLista = {
+            familia: "",
+            listaPregunta: [],
         };
         $scope.listaFam.push(auxLista);
-    }  
+    }
 
-    $scope.showPreg=function(fam){
-        $scope.mostrarPreg=true;
-        let auxPreg={
-            pregunta:"",
+    $scope.showPreg = function (fam) {
+        $scope.mostrarPreg = true;
+        let auxPreg = {
+            pregunta: "",
         };
         fam.listaPregunta.push(auxPreg);
     }
 
-    $scope.btnRubrica = function(){
+    $scope.btnRubrica = function () {
         $location.path("rubrica")
     }
 
-    $scope.irCurso = function(){
+    $scope.irCurso = function () {
         $location.path("curso")
     }
 
-    $scope.btnEstadisticas = function(){
+    $scope.btnEstadisticas = function () {
         $location.path("estadisticas")
     }
 
@@ -100,21 +100,21 @@ app.controller('ActividadController',function($scope, $location, $cookies,servic
         $location.path("comentarios")
     }
 
-    $scope.deleteFam=function(fam){
-        let pos=$scope.listaFam.indexOf(fam);
-        $scope.listaFam.splice(pos,1);
+    $scope.deleteFam = function (fam) {
+        let pos = $scope.listaFam.indexOf(fam);
+        $scope.listaFam.splice(pos, 1);
     }
 
-    $scope.deleteRow=function(fam,preg){
-        let posFam=$scope.listaFam.indexOf(fam);
-        let posPreg=$scope.listaFam[posFam].listaPregunta.indexOf(preg);
-        fam.listaPregunta.splice(posPreg,1);
+    $scope.deleteRow = function (fam, preg) {
+        let posFam = $scope.listaFam.indexOf(fam);
+        let posPreg = $scope.listaFam[posFam].listaPregunta.indexOf(preg);
+        fam.listaPregunta.splice(posPreg, 1);
     }
-    $scope.habilitarCampos=function(preg){
-        if(!preg.editar)
-        preg.editar=!(preg.editar);
+    $scope.habilitarCampos = function (preg) {
+        if (!preg.editar)
+            preg.editar = !(preg.editar);
     }
-    $scope.btnGuardarAutoEval=function(){
+    $scope.btnGuardarAutoEval = function () {
         //console.log($scope.listaFam);
         /*let params={
             idActividad:1,
@@ -134,18 +134,18 @@ app.controller('ActividadController',function($scope, $location, $cookies,servic
                 }]
             }]
         }*/
-        let params={
-            idActividad:1,
-            listaFamilia:$scope.listaFam,
+        let params = {
+            idActividad: 1,
+            listaFamilia: $scope.listaFam,
         }
         console.dir(params)
-        serviceCRUD.TypePost("auto-evaluacion/creacion",params).then(function(response){
+        serviceCRUD.TypePost("auto-evaluacion/creacion", params).then(function (response) {
             console.dir(response);
-            
+
         })
     }
 
-    $scope.btnGuardarAutoEval1=function(){
+    $scope.btnGuardarAutoEval1 = function () {
         console.log($scope.ejemplo);
     }
 })
--- app/modules/curso/RubricaController.js
+++ app/modules/curso/RubricaController.js
@@ -1,41 +1,62 @@
-app.controller('RubricaController',function($scope, $location, $cookies, serviceUtil, serviceCRUD){ 
+app.controller('RubricaController', function ($scope, $location, $cookies, serviceUtil, serviceCRUD) {
     $scope.usuario = $cookies.getObject('usuario');
     //if ($scope.usuario == undefined) $location.path('/');
 
     /* Variables */
     $scope.mostrarCrearRubrica = false;
     $scope.mostrarAspecto = true;
+    $scope.mostrarRubricaTipo1 = false;
+    $scope.mostrarRubricaTipo2 = false;
+    $scope.mostrarRubricaTipo3 = false;
 
     $("[data-toggle=tooltipOcultarAspecto]").tooltip();
 
     $scope.rubrica = {
-        flgRubricaEspecial : 0,
-        idUsuarioCreador : 2,
-        nombreRubrica : "test",
-        listaAspectos : []
+        flgRubricaEspecial: 0,
+        idUsuarioCreador: 2,
+        nombreRubrica: "test",
+        listaAspectos: []
     }
 
     /* Inicializando la lista de aspectos */
     $scope.lstAspectos = []
 
     /* Funciones Rubrica */
-    $scope.btnCrearRubrica = function(){
-        /* Preguntar si desea crear una nueva rúbrica */
-        result = window.confirm('¿Desea crear una nueva rúbrica?');
-        if (result){
-            $scope.mostrarCrearRubrica = true;
 
-        }
+    /* Función crear rúbr ica desde 0. Preguntar el tipo de rúbrica */
+    $scope.btnRubricaTipo1 = function () {
+        console.log('cambiando tipo 1');
+        $scope.mostrarRubricaTipo1 = true;
+        $scope.mostrarRubricaTipo2 = false;
+        $scope.mostrarRubricaTipo3 = false;
+    }
+    $scope.btnRubricaTipo2 = function () {
+        console.log('cambiando tipo 2');
+        $scope.mostrarRubricaTipo1 = false;
+        $scope.mostrarRubricaTipo2 = true;
+        $scope.mostrarRubricaTipo3 = false;
+    }
+    $scope.btnRubricaTipo3 = function () {
+        console.log('cambiando tipo 3');
+        $scope.mostrarRubricaTipo1 = false;
+        $scope.mostrarRubricaTipo2 = false;
+        $scope.mostrarRubricaTipo3 = true;
     }
 
-    $scope.btnGuardarRubrica = function(){
+
+    $scope.btnCrearRubrica = function () {
+        $scope.mostrarCrearRubrica = true;
+        $('#mdElegirTipoRubrica').appendTo("body").modal('show');
+    }
+
+    $scope.btnGuardarRubrica = function () {
         if (document.getElementById('nomRubrica').value == '')
             window.alert("Ingrese el nombre de la rúbrica!");
-        else{
+        else {
             $scope.mostrarCrearRubrica = false;
             console.dir($scope.rubrica);
 
-            serviceCRUD.TypePost('actividad/crear_rubrica', $scope.rubrica).then(function(response){
+            serviceCRUD.TypePost('actividad/crear_rubrica', $scope.rubrica).then(function (response) {
                 console.dir(response);
             })
 
@@ -43,67 +64,67 @@ app.controller('RubricaController',function($scope, $location, $cookies, service
         }
     }
 
-    $scope.btnVerRubricaActual = function(){
+    $scope.btnVerRubricaActual = function () {
         //Falta: Validar que primero haya guardado la rúbrica
 
         var params = {
-            idActividad : 1
+            idActividad: 1
         }
-        serviceCRUD.TypePost('actividad/obtener_rubrica_idactividad', params).then(function(res){
+        serviceCRUD.TypePost('actividad/obtener_rubrica_idactividad', params).then(function (res) {
             console.dir(res.data);
         })
 
 
-       $scope.mostrarCrearRubrica = true;
-       console.dir($scope.lstAspectos);
+        $scope.mostrarCrearRubrica = true;
+        console.dir($scope.lstAspectos);
     }
 
     /* Funciones Aspectos */
-     $scope.btnAgregarAspecto= function() {
+    $scope.btnAgregarAspecto = function () {
         $scope.rubrica.listaAspectos.push({
             /* datos del aspecto */
             descripcion: '',
             informacion: '',
             puntajeMax: null,
-            listaIndicadores:[],
+            listaIndicadores: [],
             mostrar: true,
-            tipoClasificacion : 1
+            tipoClasificacion: 1
         });
-     }
-
-    $scope.btnMostrarAspecto = function(aspecto){
-        if (!aspecto.mostrar){
-            aspecto.mostrar = !(aspecto.mostrar);   
-        }
-            
     }
 
-    $scope.btnOcultarIndicadores = function(aspecto){
-        if (aspecto.mostrar){
-            aspecto.mostrar = !(aspecto.mostrar);   
+    $scope.btnMostrarAspecto = function (aspecto) {
+        if (!aspecto.mostrar) {
+            aspecto.mostrar = !(aspecto.mostrar);
+        }
+
+    }
+
+    $scope.btnOcultarIndicadores = function (aspecto) {
+        if (aspecto.mostrar) {
+            aspecto.mostrar = !(aspecto.mostrar);
         }
     }
-    
-    $scope.btnQuitarAspecto = function(aspecto){
+
+    $scope.btnQuitarAspecto = function (aspecto) {
         var pos = $scope.rubrica.listaAspectos.indexOf(aspecto)
-        $scope.rubrica.listaAspectos.splice(pos,1)
+        $scope.rubrica.listaAspectos.splice(pos, 1)
     }
-    
+
     /* Funciones Indicadores */
-    $scope.btnAgregarIndicador= function(aspecto) {
-        aspecto.listaIndicadores.push({  
+    $scope.btnAgregarIndicador = function (aspecto) {
+        aspecto.listaIndicadores.push({
             /* datos del indicador */
             descripcion: '',
             informacion: '',
             puntajeMax: null,
-            tipo : 'NOTA'
+            tipo: 'NOTA'
         });
         console.dir($scope.rubrica.listaAspectos)
-     }
+    }
 
-     $scope.btnQuitarIndicador = function(aspecto,indicador){
+    $scope.btnQuitarIndicador = function (aspecto, indicador) {
         var pos = aspecto.listaIndicadores.indexOf(indicador)
-        aspecto.listaIndicadores.splice(pos,1)
+        aspecto.listaIndicadores.splice(pos, 1)
     }
     console.dir($scope.lstAspectos)
 
