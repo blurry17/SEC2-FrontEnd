c32b9f2e611f093ae16eaa6488fe12715c829e5e
ManuelTheDev <mjdelgado@pucp.pe>
2019-06-01 09:38:41 -0500

    comentarios ok, falta listarComentariosActividad

--- app/modules/curso/ComentariosController.js
+++ app/modules/curso/ComentariosController.js
@@ -1,23 +1,67 @@
 app.controller('ComentariosController', function ($rootScope, $scope, $location, $cookies, serviceUtil, serviceCRUD) {
     var usuario = $cookies.getObject('usuario');
+    console.dir(usuario);
     $rootScope.lstCursos = $cookies.getObject('cursos');
+    $scope.actividad = $cookies.getObject('actividadActual');
+    $scope.idActividad = $scope.actividad.idActividad;
     $scope.vistaProfesor = usuario.profesor;
 
+
+    $scope.comentario = {
+        idActividad: $scope.idActividad,
+        idAlumno: usuario.idUser,
+        comentario: ""
+    }
+
+    /* Guardar Comentario como alumno */
+    $scope.btnGuardarComentario = function () {
+        serviceCRUD.TypePost('actividad/ingresar_comentario_alumno', $scope.comentario).then(function (response) {
+            console.dir($scope.comentario.comentario);
+            console.dir(response);
+            if (response.data.error == 1)
+                window.alert(response.data.mensaje + '.\n' + "Por favor, inténtelo nuevamente");
+            else
+                window.alert("Se guardó el comentario!")
+        })
+    }
+
+    $scope.respuesta = {
+        idActividad: $scope.idActividad,
+        idAlumno : null,
+        idProfesor: usuario.idUser,
+        respuesta: ""
+    }
+
+    /* Guardar Respuesta como profesor */
+    $scope.btnGuardarRespuesta = function (comentario) {
+        //Obtengo el idAlumno del comentario y lo envio junto a la respuesta
+        $scope.respuesta.idAlumno = comentario.idAlumno;
+        serviceCRUD.TypePost('actividad/responder_comentario_alumno', $scope.respuesta).then(function (response) {
+            console.dir(response);
+            window.alert("Se guardó la respuesta!")
+        })
+    }
+
+    /* Esto no debe estar hardcodeado. Falta el servicio de listar comentariosXactividad */
     $scope.lstComentarios = [
         {
-            idComentario: 1,
+            //Este comentario como ya tiene respuesta
+            //Deberia mostrarse sin poder editar
+            idAlumno: 1,
             nomAlumno: "Alumno 1",
-            codAlumno : "19964321",
-            nomProfesor: "daniel alpiste",
-            comentario: "hola profe xd",            
+            codAlumno: "19964321",
+            nomProfesor: "Ernesto Torres",
+            comentario: "hola profe xd",
             respuesta: "ola"
         },
         {
-            idComentario: 2,
+            //Este comentario como ya tiene respuesta
+            //Deberia mostrarse para editar
+            idAlumno: 2,
             nomAlumno: "Alumno 2",
-            codAlumno : "19975432",
-            nomProfesor: "nicolas",
-            comentario: "profe no me salio la 2 ayude pues",            
+            codAlumno: "19975432",
+            nomProfesor: "Ernesto Torres",
+            comentario: "profe no me salio la 2 ayude pues",
             respuesta: ""
         }
     ]
