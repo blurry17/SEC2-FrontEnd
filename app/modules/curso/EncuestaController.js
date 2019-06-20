15094e769bccac8c3b5076b575aa20cacdbcab24
MartinRamirez98 <a20150178@pucp.pe>
2019-06-19 19:58:07 -0500

    alistando pal merge

--- app/modules/curso/EncuestaController.js
+++ app/modules/curso/EncuestaController.js
@@ -72,6 +72,7 @@ app.controller('EncuestaController', function ($rootScope, $scope, $location, $c
                 return;
             }
             $scope.rubrica = res.data;
+            console.dir($scope.rubrica);
             if(tipo == 2){
                 $scope.rubricaAuto = $scope.rubrica;
                 $scope.rubricaCoauto = null; 
@@ -89,9 +90,24 @@ app.controller('EncuestaController', function ($rootScope, $scope, $location, $c
             idActividad:$scope.actividad.idActividad,
         }
         serviceCRUD.TypePost('actividad/grupo/lista-integrantes/coevaluacion', params).then(function(res) {
-            //console.dir("ESTOOOOO")
-            //console.dir(res.data);
+            console.dir("ESTOOOOO")
+            console.dir(res.data);
             $scope.listaAl=res.data;
+        })
+    }
+
+    $scope.obtenerCo=function(){
+        let params={
+            idActividad:$scope.actividad.idActividad,
+            idCalificado:$scope.idalumno,
+            idCalificador:$scope.usuario.idUser,
+
+        }
+        console.dir('este es el yeison');
+        console.dir(params);
+        serviceCRUD.TypePost('coevaluacion/obtener_coevaluacion',params).then(function(res){
+            console.dir('LA RES');
+            console.dir(res.data);
         })
     }
 
@@ -102,11 +118,14 @@ app.controller('EncuestaController', function ($rootScope, $scope, $location, $c
             idCalificador:$scope.usuario.idUser,
             nota:0,
             flgFalta:0,
-            listaNotaAspectos:[
-
-            ],
+            listaNotaAspectos:$scope.rubrica.listaAspectos,
             flgCompleto:0,
         }
+        console.dir('LEEEE ESTO');
+        console.dir(params);
+        serviceCRUD.TypePost('coevaluacion/calificar_coevaluacion',params).then(function(res){
+
+        })
     }
 
     $scope.btnGuardarEvaluacion = function (tipo) {
