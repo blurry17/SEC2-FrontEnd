f97fc0f558983f92d1964d8f7c8295fbb66c46e2
Hugo Velarde Vigo <u07900@PER.BNS>
2019-06-28 15:49:31 -0500

    cambio lista notas

--- app/modules/curso/ListarNotasController.js
+++ app/modules/curso/ListarNotasController.js
@@ -5,24 +5,33 @@ app.controller('ListarNotasController', function ($rootScope, $scope, $location,
     $rootScope.lstCursos = $cookies.getObject('cursos');
     $scope.curso = $cookies.getObject("cursoActual");
     $scope.actividad = $cookies.getObject("actividadActual");
-    $scope.rubrica = $cookies.getObject("rubricaActual");
+    //$scope.rubrica = $cookies.getObject("");
     $scope.esProfesor = $scope.usuario.profesor;
     $scope.listaNotasAlum =[];
 
- 
-    function ListarNotas() {
-        var params = { 
+  
+    function ObtenerRubrica() {
+      var params = {
           idActividad: $scope.actividad.idActividad,
-          idRubrica: $scope.rubrica.idRubrica }
-        
-          //console.dir(params);
-          serviceCRUD.TypePost('notas-finales', params).then(function (res) {
-          $scope.listaNotasAlum = res.data.listaNotas;
-          //console.dir("holaaaa");
-        })
-    }
+          tipo: 4,
+      }
+      serviceCRUD.TypePost('actividad/obtener_rubrica', params).then(function (res) {
+          $scope.idRub = res.data.idRubrica;
+
+          var params = { 
+            idActividad: $scope.actividad.idActividad,
+            idRubrica: $scope.idRub }
+          
+            //console.dir(params);
+            serviceCRUD.TypePost('notas-finales', params).then(function (res) {
+            $scope.listaNotasAlum = res.data.listaNotas;
+            //console.dir("holaaaa");
+          })
+      })
+  }
+    
     function init() {
-        ListarNotas();
+      ObtenerRubrica();
       }
     
       init();
